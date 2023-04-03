65cc53508bcbebcb7c335f0a5d263f577f27d8b9
olamic <olawuyim338@gmail.com>
2023-04-03 08:21:11 -0700

    small updtae

--- webinterface/models.py
+++ webinterface/models.py
@@ -15,8 +15,9 @@ LABEL_CHOICES = (
 
 )
 
+
 class Item(models.Model):
-    title = models.CharField(max_length=150)
+    title = models.CharField(max_length=100)
     price = models.FloatField()
     category = models.CharField(choices=CATEGORY_CHOICES, max_length=150)
     label = models.CharField(choices=LABEL_CHOICES, max_length=2)
@@ -41,3 +42,9 @@ class Order(models.Model):
 
     def __str__(self):
         return self.user.username
+
+    def get_items(items):
+        try:
+            return Order.objects.get(items=items)
+        except:
+            return False
